a70f836af1bd4b877d813346a6623a4db44bd6b6
CodeCat <vldtpk@gmail.com>
2025-07-23 20:17:41 +0300

    Added some thing to expertise

--- src/sections/Expertise.tsx
+++ src/sections/Expertise.tsx
@@ -1,29 +1,48 @@
 import classNames from "classnames";
 import { defaultContainer } from "../stylizers";
 
-const Expertise: React.FC = () => {
-  const block = classNames(defaultContainer());
+interface DescriptonBlockProps {
+  title?: string;
+  classname?: string;
+  children?: React.ReactNode;
+}
 
+const DescriptonBlock: React.FC<DescriptonBlockProps> = ({
+  title,
+  classname,
+  children,
+}) => {
+  const block = classNames(defaultContainer(), classname);
+  return (
+    <div className={block}>
+      <h2 className="uppercase text-2xl">{title}</h2>
+      <p className="text-xl">{children}</p>
+    </div>
+  );
+};
+
+const Expertise: React.FC = () => {
   return (
     <section id="experience" className="p-8 flex justify-end">
-      <div className="flex flex-wrap">
-        <div className={block}>
-          <h2>Character Info</h2>
-        </div>
-        <div className={block}>
-          <h2>Portrait</h2>
-        </div>
+      <div className="flex flex-wrap max-w-xl">
+        <DescriptonBlock title="Character Info" classname="grow">
+          <>
+            name: Vladislav S.
+            <br />
+            status: Open to work
+            <br />
+            experience: 4+ years
+            <br />
+            titles: Software Engineer, Frontend Developer, Excellent Problem
+            Solver
+          </>
+        </DescriptonBlock>
+        <DescriptonBlock title="Portrait" classname="w-[200px]" />
         <div className="w-full" />
-        <div className={block}>
-          <h2>Frontend</h2>
-        </div>
+        <DescriptonBlock title="Frontend" classname="grow" />
         <div className="w-full" />
-        <div className={block}>
-          <h2>Software Engineer</h2>
-        </div>
-        <div className={block}>
-          <h2>Miscellaneous</h2>
-        </div>
+        <DescriptonBlock title="Software Engineer" classname="grow" />
+        <DescriptonBlock title="Miscellaneous" classname="grow" />
       </div>
     </section>
   );
