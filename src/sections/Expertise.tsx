5df0c07252100a7b156ba0521d03dc0c1bc756c6
C0deCat <51781844+C0deCat@users.noreply.github.com>
2025-07-23 21:42:45 +0300

    feat(expertise): update layout

--- src/sections/Expertise.tsx
+++ src/sections/Expertise.tsx
@@ -15,8 +15,8 @@ const DescriptonBlock: React.FC<DescriptonBlockProps> = ({
   const block = classNames(defaultContainer(), classname);
   return (
     <div className={block}>
-      <h2 className="uppercase text-2xl">{title}</h2>
-      <p className="text-xl">{children}</p>
+      <h2 className="uppercase text-[4cqw]">{title}</h2>
+      <p className="text-[3cqw]">{children}</p>
     </div>
   );
 };
@@ -24,25 +24,40 @@ const DescriptonBlock: React.FC<DescriptonBlockProps> = ({
 const Expertise: React.FC = () => {
   return (
     <section id="experience" className="p-8 flex justify-end">
-      <div className="flex flex-wrap max-w-xl">
-        <DescriptonBlock title="Character Info" classname="grow">
-          <>
-            name: Vladislav S.
-            <br />
-            status: Open to work
-            <br />
-            experience: 4+ years
-            <br />
-            titles: Software Engineer, Frontend Developer, Excellent Problem
-            Solver
-          </>
-        </DescriptonBlock>
-        <DescriptonBlock title="Portrait" classname="w-[200px]" />
-        <div className="w-full" />
-        <DescriptonBlock title="Frontend" classname="grow" />
-        <div className="w-full" />
-        <DescriptonBlock title="Software Engineer" classname="grow" />
-        <DescriptonBlock title="Miscellaneous" classname="grow" />
+      <div
+        id="cardWrapper"
+        className="flex flex-col flex-wrap [container-type:inline-size]"
+        style={{ width: "min(max(40vw,36rem),100vw)" }}
+      >
+        <div className="flex flex-wrap items-stretch w-full">
+          <DescriptonBlock title="Character Info" classname="grow">
+            <>
+              name: Vladislav S.
+              <br />
+              status: Open to work
+              <br />
+              experience: 4+ years
+              <br />
+              titles: Software Engineer, Frontend Developer, Excellent Problem
+              Solver
+            </>
+          </DescriptonBlock>
+          <DescriptonBlock
+            title="Portrait"
+            classname="aspect-square self-stretch min-w-[200px] min-h-[200px]"
+          />
+        </div>
+        <DescriptonBlock title="Frontend" classname="w-full" />
+        <div className="flex flex-wrap w-full">
+          <DescriptonBlock
+            title="Software Engineer"
+            classname="flex-1 min-w-[250px]"
+          />
+          <DescriptonBlock
+            title="Miscellaneous"
+            classname="flex-1 min-w-[250px]"
+          />
+        </div>
       </div>
     </section>
   );
