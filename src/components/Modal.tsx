a513bbbb616032da31646f4be70ab43acc7e8ee8
CodeCat <vldtpk@gmail.com>
2025-07-27 14:42:29 +0300

    visual adjustment for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import classNames from "classnames";
+import React, { useEffect } from "react";
+import { defaultContainer } from "../stylizers";
 
 interface ModalProps {
   title: string;
@@ -8,22 +10,38 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ title, stack, content, onClose }) => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
+  const modalClasses = classNames(
+    defaultContainer(false),
+    "relative border-0 overflow-hidden w-full h-full"
+  );
+
   return (
-    <div className="fixed inset-0 z-20 bg-black/80 flex items-start justify-center">
-      <div className="relative bg-(--bg-color) text-primary w-full h-full overflow-hidden">
-        <header className="sticky top-0 flex justify-between items-center p-4 border-b border-(--color-primary) bg-(--color-primary-dark)/90 backdrop-blur-sm z-10">
-          <h2 className="text-2xl">{title}</h2>
-          <button className="text-3xl leading-none" onClick={onClose}>&times;</button>
-        </header>
-        <div className="p-4 overflow-auto h-full">
-          <aside className="float-right w-[150px] ml-4 mb-4 border-l border-(--color-primary) pl-4">
-            <ul className="space-y-1">
-              {stack.map((item) => (
-                <li key={item}>#{item}</li>
-              ))}
-            </ul>
-          </aside>
-          <div>{content}</div>
+    <div className="fixed inset-0 z-20 flex items-start p-5 justify-center">
+      <div className={modalClasses}>
+        <div className="border-corner h-full">
+          <header className="sticky top-0 flex justify-between items-center p-4 pt-2 border-(--color-primary) z-10">
+            <h2 className="text-4xl">{title}</h2>
+            <button className="text-3xl leading-none" onClick={onClose}>
+              &times;
+            </button>
+          </header>
+          <div className="p-4 overflow-auto h-full">
+            <aside className="float-right w-[150px] ml-4 mb-4 border-l border-(--color-primary) pl-4">
+              <ul className="space-y-1">
+                {stack.map((item) => (
+                  <li key={item}>#{item}</li>
+                ))}
+              </ul>
+            </aside>
+            <div>{content}</div>
+          </div>
         </div>
       </div>
     </div>
