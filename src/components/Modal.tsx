6e30c944a0b195ad7ae09b5863851c9daf553d9a
CodeCat <vldtpk@gmail.com>
2025-07-30 00:58:08 +0300

    adjustments to modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,28 +26,27 @@ const Modal: React.FC<ModalProps> = ({ title, stack, content, onClose }) => {
   return (
     <div className="fixed inset-0 z-20 flex items-start p-5 justify-center">
       <div className={modalClasses}>
-        <div className="border-corner h-full overflow-hidden">
+        <div className="border-corner h-full overflow-hidden flex flex-col">
           <button
             className="text-3xl leading-none absolute right-[10px] top-[10px] text-(--color-primary) cursor-pointer z-20"
             onClick={onClose}
           >
             <CloseIcon />
           </button>
-          <header className="sticky top-0 flex justify-between items-center p-11 pt-7 pb-0 border-(--color-primary) pr-32 z-10">
+          <header className="top-0 flex justify-between items-center p-11 pt-7 border-(--color-primary) pr-32 z-10">
             <h2 className="text-4xl">{title}</h2>
             <span className="text-2xl">open project {"-->"}</span>
           </header>
-          <div className="overflow-auto">
-            <div className="p-11">
-              <aside className="float-right w-[150px] ml-4 mb-4 border-l border-(--color-primary) pl-4">
-                <ul className="space-y-1">
-                  {stack.map((item) => (
-                    <li key={item}>#{item}</li>
-                  ))}
-                </ul>
-              </aside>
-              <div>{content}</div>
-            </div>
+          <div className="pr-11 pl-11 pb-11 overflow-auto text-2xl">
+            <aside className="float-right min-w-[170px] bg-(--color-secondary) ml-4 mb-4 back p-4">
+              <p className="mb-4">Stack</p>
+              <ul>
+                {stack.map((item) => (
+                  <li key={item}>{item}</li>
+                ))}
+              </ul>
+            </aside>
+            <div>{content}</div>
           </div>
         </div>
       </div>
